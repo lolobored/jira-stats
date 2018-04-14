44260692fd3b6b57a3899011ca03a95400bb236f
Laurent Laborde <llaborde@bottomline.com>
2018-04-15 00:55:47 +0100

    Fixes for LocalDateTime serialization

--- jira-objects/src/main/java/org/lolobored/jira/model/Sprint.java
+++ jira-objects/src/main/java/org/lolobored/jira/model/Sprint.java
@@ -1,6 +1,10 @@
 package org.lolobored.jira.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,7 +14,11 @@ import java.time.LocalDateTime;
 public class Sprint {
   private Integer id;
   private String name;
+  @JsonDeserialize(using = LocalDateDeserializer.class)
+  @JsonSerialize(using = LocalDateSerializer.class)
   private LocalDateTime startDate;
+  @JsonDeserialize(using = LocalDateDeserializer.class)
+  @JsonSerialize(using = LocalDateSerializer.class)
   private LocalDateTime endDate;
 
 }
--- jira-objects/src/main/java/org/lolobored/jira/model/Worklog.java
+++ jira-objects/src/main/java/org/lolobored/jira/model/Worklog.java
@@ -2,6 +2,10 @@ package org.lolobored.jira.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Data;
 
 import java.math.BigInteger;
@@ -11,6 +15,8 @@ import java.time.LocalDateTime;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Worklog {
 
+  @JsonDeserialize(using = LocalDateDeserializer.class)
+  @JsonSerialize(using = LocalDateSerializer.class)
   private LocalDateTime updated;
   private BigInteger timeSpentSeconds;
 
